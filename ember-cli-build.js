8c57f45c90f202c92a546f875ea6970ea7219c1e
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-11-13 22:04:54 +1100

    Add SASS, fingerprint, minify, prefix and fetch configs to build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,72 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const buildConfig = require('./config/environment')(EmberApp.env()).buildConfig;
+
+const { isProduction, gitRevision } = buildConfig;
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
-  });
+    // Makes SASS listen to file changes in the component folders
+    sassOptions: {
+      includePaths: ['app/components'],
+      overwrite: true
+    },
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+    // Adds CSS browser prefixes
+    autoprefixer: {
+      browsers: [
+        '> 1%',
+        'Explorer > 10',
+        'Firefox >= 17',
+        'Chrome >= 10',
+        'Safari >= 6',
+        'iOS >= 6'
+      ],
+      cascade: false,
+      remove: false
+    },
+
+    // Prevent CSS minification in development and tests
+    minifyCSS: {
+      enabled: isProduction
+    },
+
+    // Prevent JS minification in development and tests
+    minifyJS: {
+      enabled: isProduction
+    },
+
+    // Enable source maps for debugging and Sentry
+    sourcemaps: {
+      enabled: isProduction,
+      extensions: ['js']
+    },
+
+    fingerprint: {
+      enabled: isProduction,
+      extensions: [
+        'js',
+        'css',
+        'png',
+        'jpg',
+        'svg',
+        'map',
+        'mp4',
+        'ogg',
+        'webp',
+        'webm',
+        'woff',
+        'woff2'
+      ],
+      replaceExtensions: ['html', 'css', 'scss', 'js'],
+      customHash: gitRevision
+    },
+
+    'ember-fetch': {
+      preferNative: true
+    }
+  });
 
   return app.toTree();
 };
