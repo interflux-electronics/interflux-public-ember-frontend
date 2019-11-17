15f7c940aa63a7ae141c04c9de3564a797cae1ed
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-11-17 12:16:13 +1100

    Solve ERR_CLEARTEXT_NOT_PERMITTED and ERR_CONNECTION_REFUSED in Corber

--- config/targets.js
+++ config/targets.js
@@ -1,16 +1,47 @@
+// Out of the box Ember
+//
+// 'use strict';
+//
+// const browsers = [
+//   'last 1 Chrome versions',
+//   'last 1 Firefox versions',
+//   'last 1 Safari versions'
+// ];
+//
+// const isCI = !!process.env.CI;
+// const isProduction = process.env.EMBER_ENV === 'production';
+//
+// if (isCI || isProduction) {
+//   browsers.push('ie 11');
+// }
+//
+// module.exports = {
+//   browsers
+// };
+
+// As suggested by Corber
+// http://corber.io/pages/frameworks/ember#configuring-browser-targets
+
 'use strict';
 
-const browsers = [
-  'last 1 Chrome versions',
-  'last 1 Firefox versions',
-  'last 1 Safari versions'
-];
+let browsers;
 
-const isCI = !!process.env.CI;
-const isProduction = process.env.EMBER_ENV === 'production';
+if (process.env.CORBER) {
+  browsers = [`last 1 ${process.env.CORBER_PLATFORM} versions`];
+} else {
+  // out-of-the-box ember-cli behaviour
+  browsers = [
+    'last 1 Chrome versions',
+    'last 1 Firefox versions',
+    'last 1 Safari versions'
+  ];
 
-if (isCI || isProduction) {
-  browsers.push('ie 11');
+  const isCI = !!process.env.CI;
+  const isProduction = process.env.EMBER_ENV === 'production';
+
+  if (isCI || isProduction) {
+    browsers.push('ie 11');
+  }
 }
 
 module.exports = {
