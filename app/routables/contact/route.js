0a9c3bcee90d9a2acd1f294dce99f67ee0c2fc8c
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-04-05 11:31:09 +1000

    Show companies on the contact page

--- app/routables/contact/route.js
+++ app/routables/contact/route.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default class ContactRoute extends Route {
+  @service store;
+
+  model() {
+    return hash({
+      companies: this.store.findAll('company'),
+      countries: this.store.findAll('country')
+    });
+  }
 }
