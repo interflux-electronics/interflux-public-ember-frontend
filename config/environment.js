6b9c6b298fba6f4bff69946170e978f511b045c2
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-04-05 16:59:38 +1000

    Add Mapbox access token

--- config/environment.js
+++ config/environment.js
@@ -114,7 +114,11 @@ module.exports = function(env) {
     apiNamespace,
     gitBranch,
     gitRevision,
-    themeColour
+    themeColour,
+
+    'mapbox-gl': {
+      accessToken: `pk.eyJ1IjoianctZmxvYXRwbGFuZS1kZXYiLCJhIjoiY2s4bW02N3UyMG93MTNycGduNzJqOGt6OCJ9.PHUKAn3CMmN73tmJXpa0ug`
+    }
   };
 
   if (isTest) {
