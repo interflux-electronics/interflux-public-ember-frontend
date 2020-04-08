8eae1ef9f5cf6d4f6f060338901e62b2a90fdf7e
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-04-09 09:43:06 +1000

    Add <LoadingSpinner> components and loading state to <Image::Responsive>

--- app/routables/components/image/responsive/component.js
+++ app/routables/components/image/responsive/component.js
@@ -5,32 +5,23 @@ import { action } from '@ember/object';
 // TODO: write tests
 
 export default class ImageResponsiveComponent extends Component {
+  @tracked foundOptimal = false;
   @tracked loading = true;
-  @tracked loadFailed = false;
-  @tracked showImage = false;
-  @tracked showError = false;
+  @tracked error = false;
   @tracked optimalWidth;
   @tracked optimalHeight;
 
-  get webp() {
-    return false; // TODO: first fix chroma issue with WEBP images
-    // return this.args.formats && this.args.formats.includes('webp');
+  get validParams() {
+    return this.args.sizes && this.args.formats && this.args.path;
   }
 
-  @action
-  didInsert(element) {
-    this.computeOptimalSize(element);
-  }
-
-  // TODO: Stop watching
-  // @action
-  // willDestroy(element) {
-  //   console.debug('<Image::Responsive> willDestroy()', { element });
-  // }
-
+  // From the moment the <figure> element is inserted into the DOM, we know its
+  // width and can then fire the computeOptimalSize() action to find the image
+  // with the smallest possible resolution which renders a sharp image. This
+  // technique can dramatically reduce download times, especially on mobile.
   @action
   computeOptimalSize(element) {
-    const sizes = this.args.sizes;
+    const { sizes } = this.args;
     const pixelRatio = window.devicePixelRatio || 1;
     const optimalWidth = element.offsetWidth * pixelRatio;
 
@@ -43,7 +34,7 @@ export default class ImageResponsiveComponent extends Component {
     const smaller = distances.filter(d => d < 0);
 
     const closestDistance = larger.length
-      ? Math.max(...larger)
+      ? Math.min(...larger)
       : Math.max(...smaller);
 
     const closestSize = sizes.find(size => {
@@ -51,22 +42,27 @@ export default class ImageResponsiveComponent extends Component {
       return width - optimalWidth === closestDistance;
     });
 
-    this.optimalHeight = closestSize.split('x')[0];
-    this.optimalWidth = closestSize.split('x')[1];
-    this.showImage = true;
+    this.optimalWidth = closestSize.split('x')[0];
+    this.optimalHeight = closestSize.split('x')[1];
+    this.foundOptimal = true;
+  }
+
+  get webp() {
+    // return false;
+    // TODO: first fix chroma issue with WEBP images
+    return this.args.formats.includes('webp');
   }
 
   @action
   onLoad() {
-    // console.debug('<Image::Responsive> onLoad()');
     this.loading = false;
-    this.loadFailed = false;
+    this.error = false;
   }
 
   @action
-  onError(error) {
-    console.error('<Image::Responsive> onError()', { error });
+  onError() {
     this.loading = false;
-    this.loadFailed = true;
+    this.error = true;
+    console.error('Failed to load image', this.args.path);
   }
 }
