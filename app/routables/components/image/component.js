71a9cb649cac0f98322a048b92fe5773b4f009b9
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-04-12 14:55:39 +1000

    Add <Image> component, stubbed product feature icons and fix thumb load bug

---
+++ app/routables/components/image/component.js
@@ -0,0 +1,21 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class ImageComponent extends Component {
+  @tracked loading = true;
+  @tracked error = false;
+
+  @action
+  onLoad() {
+    this.loading = false;
+    this.error = false;
+  }
+
+  @action
+  onError() {
+    this.loading = false;
+    this.error = true;
+    console.error('Failed to load image', this.args.image.path);
+  }
+}
