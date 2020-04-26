c595922838e0400d6727e949dc63f8eb36766bc0
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-04-26 16:09:25 +1000

    Hide non-featured products and add hover over state to product tiles

--- app/routables/components/section/product-tiles/component.js
+++ app/routables/components/section/product-tiles/component.js
@@ -1,3 +1,17 @@
 import Component from '@glimmer/component';
 
-export default class SectionProductTilesComponent extends Component {}
+export default class SectionProductTilesComponent extends Component {
+  get sortedProducts() {
+    const featured = this.args.products
+      .filterBy('featured')
+      .sortBy('order', 'name');
+
+    const hidden = this.args.products
+      .rejectBy('featured')
+      .sortBy('order', 'name');
+
+    const arr = [...featured, ...hidden];
+
+    return arr;
+  }
+}
