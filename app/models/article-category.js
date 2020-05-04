15abd1955a4d7076fa6dee45b027f5f4d9931a3d
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-05-04 16:56:14 +1000

    Add responsive CSS to document category modals

--- app/models/article-category.js
+++ app/models/article-category.js
@@ -1,7 +1,8 @@
 import Model, { attr, hasMany } from '@ember-data/model';
+import { alias } from '@ember/object/computed';
 
 export default class ArticleCategoryModel extends Model {
-  @attr('string') slug;
+  @alias('id') slug;
   @attr('string') name;
 
   @hasMany('article') articles;
