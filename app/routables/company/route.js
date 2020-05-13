a212a24fb810da832eec2f9ee750fbf08ce74604
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-05-13 19:05:46 +1000

    Add automatic year counter

--- app/routables/company/route.js
+++ app/routables/company/route.js
@@ -1,4 +1,3 @@
 import Route from '@ember/routing/route';
 
-export default class HomeCompanyRoute extends Route {
-}
+export default class CompanyRoute extends Route {}
--- app/routables/components/footer/component.js
+++ app/routables/components/footer/component.js
@@ -2,6 +2,6 @@ import Component from '@glimmer/component';
 
 export default class FooterComponent extends Component {
   get year() {
-    return '2020';
+    return new Date().getFullYear();
   }
 }
