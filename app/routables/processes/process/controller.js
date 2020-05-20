9350461564a8f58cd19e32410184b0e912ac76d1
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-05-20 14:01:27 +1000

    Add responsive CSS for process pages

--- app/routables/processes/process/controller.js
+++ app/routables/processes/process/controller.js
@@ -1,6 +1,9 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default class ProcessesProcessController extends Controller {
+  @service media;
+
   get processes() {
     return this.model.processes.filterBy('hasPage', true);
   }
