d019593616df7463d8ce38bd2c5f180ddf278fef
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-05-20 14:00:59 +1000

    Add Ember responsive service

---
+++ app/breakpoints.js
@@ -0,0 +1,6 @@
+export default {
+  mobile: '(max-width: 600px)',
+  tablet: '(min-width: 601px) and (max-width: 900px)',
+  desktop: '(min-width: 901px) and (max-width: 1200px)',
+  widescreen: '(min-width: 1201px)'
+};
