a654374ea9b1d11c603d58331e14351a08971e83
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-11-21 19:53:33 +1100

    Make the CSS environment aware

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,12 +3,25 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const ENV = require('./config/environment')(EmberApp.env());
 
+// To make CSS environment aware we assign a new src to output map.
+// The src changes with the environment.
+const cssMap = {};
+
+cssMap[ENV.environment] = `/assets/app.css`;
+
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    outputPaths: {
+      app: {
+        css: cssMap
+      }
+    },
+
     // Makes SASS listen to file changes in the component folders
     sassOptions: {
       includePaths: ['app/pods'],
-      overwrite: true
+      overwrite: true,
+      sourceMap: false
     },
 
     // Adds CSS browser prefixes
