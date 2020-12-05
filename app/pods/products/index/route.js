7471f231a07fe35ac60a103a9e3a51236d91c31d
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-12-05 19:36:42 +1100

    Fix the products index page

--- app/pods/products/index/route.js
+++ app/pods/products/index/route.js
@@ -1,13 +1,11 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import { hash } from 'rsvp';
+// import { inject as service } from '@ember/service';
+// import { hash } from 'rsvp';
 
 export default class ProductsIndexRoute extends Route {
-  @service store;
-
-  model() {
-    return hash({
-      families: this.store.peekAll('productFamily').sortBy('rank', 'namePlural')
-    });
-  }
+  // model() {
+  //   return hash({
+  //     families: this.store.peekAll('productFamily').sortBy('rank', 'namePlural')
+  //   });
+  // }
 }
