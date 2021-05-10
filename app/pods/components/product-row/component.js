7437d1b96d3dbf98a15bbab314017f7e014e94c1
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-05-10 18:57:24 +1000

    Add guards to product row

--- app/pods/components/product-row/component.js
+++ app/pods/components/product-row/component.js
@@ -2,14 +2,18 @@ import Component from '@glimmer/component';
 
 export default class ProductRowComponent extends Component {
   get features() {
-    const uses = this.args.product.productUsesSorted
-      .filterBy('showOnProductList')
-      .mapBy('use');
+    const { product } = this.args;
 
-    const qualities = this.args.product.productQualitiesSorted
-      .filterBy('showOnProductList')
-      .mapBy('quality');
+    const uses = product.productUsesSorted
+      ? product.productUsesSorted.filterBy('showOnProductList').mapBy('use')
+      : [];
 
-    return [...uses, ...qualities].slice(0, 5);
+    const qualities = product.productQualitiesSorted
+      ? product.productQualitiesSorted
+          .filterBy('showOnProductList')
+          .mapBy('quality')
+      : [];
+
+    return [...uses, ...qualities];
   }
 }
