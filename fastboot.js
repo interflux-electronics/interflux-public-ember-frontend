9021fd935d21a4b6bef78df17a6e47297ddfded1
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-05-16 17:25:57 +1000

    Add Node script for running Fastboot

---
+++ fastboot.js
@@ -0,0 +1,24 @@
+/*eslint-env node*/
+
+const FastBootAppServer = require('fastboot-app-server');
+const FastBootWatchNotifier = require('fastboot-watch-notifier');
+
+const distPath = 'dist';
+
+const notifier = new FastBootWatchNotifier({
+  distPath,
+  debounceDelay: 250,
+  saneOptions: {
+    poll: true
+  }
+});
+
+const server = new FastBootAppServer({
+  distPath,
+  notifier,
+  gzip: true,
+  host: '0.0.0.0',
+  port: 8000
+});
+
+server.start();
