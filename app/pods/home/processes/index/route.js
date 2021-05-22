1ddfe937484eeafbdca5f7748b95fb7c3066a830
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-05-22 22:22:31 +1000

    Remove unused article and process routes

--- app/pods/home/processes/index/route.js
+++
@@ -1,9 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class ProcessesIndexRoute extends Route {
-  model() {
-    return {
-      processes: this.modelFor('processes').processes
-    };
-  }
-}
