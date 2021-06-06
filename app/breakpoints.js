272f13af811a5246962f13ca92fc04bbf64df818
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-06-06 16:24:25 +1000

    Add joint responsive breakpoints

--- app/breakpoints.js
+++ app/breakpoints.js
@@ -2,5 +2,7 @@ export default {
   mobile: '(max-width: 600px)',
   tablet: '(min-width: 601px) and (max-width: 900px)',
   desktop: '(min-width: 901px) and (max-width: 1200px)',
-  widescreen: '(min-width: 1201px)'
+  widescreen: '(min-width: 1201px)',
+  mobileTablet: '(max-width: 900px)',
+  desktopWidescreen: '(min-width: 901px)'
 };
