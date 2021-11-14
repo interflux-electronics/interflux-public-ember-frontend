eae3198381699f5842b4ac1c89055a88f135b22f
Jan Werkhoven <jan.werkhoven@gmail.com>
2021-11-14 23:15:04 +1100

    Add the service to control <Page>

--- app/pods/components/page/component.js
+++ app/pods/components/page/component.js
@@ -3,11 +3,34 @@ import { inject as service } from '@ember/service';
 import { htmlSafe } from '@ember/template';
 
 export default class PageComponent extends Component {
+  @service page;
   @service modal;
   @service media;
 
+  get id() {
+    return this.page.id || this.args.id;
+  }
+
+  get title() {
+    return this.page.title || this.args.title;
+  }
+
+  get backRoute() {
+    return this.page.backRoute || this.args.backRoute;
+  }
+
+  get backModel() {
+    return this.page.backModel || this.args.backModel;
+  }
+
   get pageClasses() {
     return this.modal.showModal ? 'no-scroll' : null;
+  }
+
+  get mainClasses() {
+    return [this.page.mainClasses, this.args.class]
+      .filter((x) => !!x)
+      .join(' ');
   }
 
   get pageStyle() {
