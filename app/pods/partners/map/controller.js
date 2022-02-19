0f3c7da494565ed0c9e609a9196df5d1616447bd
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-02-20 02:23:10 +1100

    Use query params for map navigations, smoothen zoom interactions

--- app/pods/partners/map/controller.js
+++ app/pods/partners/map/controller.js
@@ -1,14 +1,16 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 
 export default class PartnersMapController extends Controller {
-  // @model countries
-  // @model companies
-  // @model slug
-
   @service mapbox;
+  @service router;
   @service window;
+
+  queryParams = ['show'];
+
+  @tracked show = null;
 
   get countries() {
     return this.model.countries;
@@ -19,12 +21,32 @@ export default class PartnersMapController extends Controller {
   }
 
   get company() {
-    return this.companies.find((c) => c.slug === this.model.slug);
+    return this.show ? this.companies.find((c) => c.slug === this.show) : null;
   }
 
-  @action onInsertMap() {
+  @action
+  hideCompany() {
+    this.zoomOutFromCompany(this.company);
+    this.show = null;
+  }
+
+  @action
+  onInsertMap() {
     console.debug('inserted map');
     this.waitForMapBoxReady();
+  }
+
+  @action
+  onMarkerClick(company, event) {
+    console.debug('onMarkerClick');
+    this.centerOnCompanyAndZoomIn(company);
+    console.debug('transition');
+    this.router.transitionTo('partners.map', {
+      queryParams: { show: company.slug }
+    });
+
+    // Prevents map clicks from hiding the company before it showed.
+    event.stopPropagation();
   }
 
   async waitForMapBoxReady() {
@@ -34,6 +56,9 @@ export default class PartnersMapController extends Controller {
       if (window.mapboxgl) {
         ready = true;
         this.renderMap();
+        if (this.company) {
+          this.centerOnCompanyAndZoomIn(this.company);
+        }
       }
       await this.window.delay(100);
     }
@@ -55,7 +80,21 @@ export default class PartnersMapController extends Controller {
       container: 'mapbox',
       style: 'mapbox://styles/jw-floatplane-dev/ck8mcsfr50uwe1iohs6xv6n0d',
       center,
-      zoom: 2
+      zoom: 3,
+      maxZoom: 16,
+      minZoom: 3,
+      pitchWithRotate: false,
+      touchPitch: false,
+      touchZoomRotate: false
+    });
+
+    this.mapbox.map.on('click', () => {
+      console.log('click');
+      console.log(this.company);
+      if (this.company) {
+        console.log('HIDE');
+        this.hideCompany();
+      }
     });
 
     const nav = new window.mapboxgl.NavigationControl({
@@ -87,5 +126,22 @@ export default class PartnersMapController extends Controller {
     // const marker2 = new window.mapboxgl.Marker({ color: 'black', rotation: 45 })
     //   .setLngLat([12.65147, 55.608166])
     //   .addTo(map);
+  }
+
+  centerOnCompanyAndZoomIn(company) {
+    this.mapbox.map.easeTo({
+      center: { lon: company.longitude, lat: company.latitude },
+      offset: [60, 0],
+      zoom: 11,
+      duration: 2000
+    });
+  }
+
+  zoomOutFromCompany(company) {
+    this.mapbox.map.easeTo({
+      center: { lon: company.longitude, lat: company.latitude },
+      zoom: 5,
+      duration: 2000
+    });
   }
 }
