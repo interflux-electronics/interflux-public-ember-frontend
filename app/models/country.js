c5ce30dff409a7cfc15e731407917969241a1fee
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-03-02 16:53:46 +0100

    Remove unneeded properties from Country model

--- app/models/country.js
+++ app/models/country.js
@@ -3,18 +3,8 @@ import Model, { attr, hasMany } from '@ember-data/model';
 export default class CountryModel extends Model {
   @attr('string') nameEnglish;
   @attr('string') nameNative;
-  @attr('string') threeLetterCode;
-  @attr('string') numericCode;
-  @attr('string') region;
-  @attr('string') subregion;
   @attr('number') latitude;
   @attr('number') longitude;
-  @attr('number') area;
-  @attr('number') population;
-  @attr('string') flagUrl;
-  @attr('array') timezones;
-  @attr('array') topLevelDomains;
-  @attr('array') callingCodes;
 
   get name() {
     return (
