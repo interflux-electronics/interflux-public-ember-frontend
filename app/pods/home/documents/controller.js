ebf9ed587ac15195fec9223a2529719f0baca177
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-03-09 02:29:18 +0100

    Omit webinars and presentations from documents page

--- app/pods/home/documents/controller.js
+++ app/pods/home/documents/controller.js
@@ -2,6 +2,9 @@ import Controller from '@ember/controller';
 
 export default class DocumentsController extends Controller {
   get categories() {
-    return this.model.categories.sortBy('order');
+    return this.model.categories
+      .rejectBy('slug', 'presentations')
+      .rejectBy('slug', 'webinars')
+      .sortBy('order');
   }
 }
