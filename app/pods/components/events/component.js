ce8a98f4d38974ca8279747af723c76db4d3380e
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-11-09 11:06:16 +1100

    Add Interflux events at the bottom of all pages

---
+++ app/pods/components/events/component.js
@@ -0,0 +1,62 @@
+import Component from '@glimmer/component';
+
+export default class EventsComponent extends Component {
+  get events() {
+    return [
+      {
+        date: '28 Oct 2022',
+        title: 'Ersa soldering technology seminar',
+        location: 'Munich, Germany',
+        cta: 'meet Steven Teliszewski'
+      },
+      {
+        date: '12 – 14 Nov 2022',
+        title: 'Matalec',
+        location: 'Madrid, Spain',
+        cta: 'meet Interflux at boot B302'
+      },
+      {
+        date: '6 Dec 2022',
+        title: 'Electronex',
+        location: 'Melbourne, Australia',
+        cta: 'meet Oritech at stand 24'
+      },
+      {
+        date: '28 Oct 2022',
+        title: 'Ersa soldering technology seminar',
+        location: 'Munich, Germany',
+        cta: 'meet Steven Teliszewski'
+      },
+      {
+        date: '12 – 14 Nov 2022',
+        title: 'Matalec',
+        location: 'Madrid, Spain',
+        cta: 'meet Interflux at boot B302'
+      },
+      {
+        date: '6 Dec 2022',
+        title: 'Electronex',
+        location: 'Melbourne, Australia',
+        cta: 'meet Oritech at stand 24'
+      },
+      {
+        date: '28 Oct 2022',
+        title: 'Ersa soldering technology seminar',
+        location: 'Munich, Germany',
+        cta: 'meet Steven Teliszewski'
+      },
+      {
+        date: '12 – 14 Nov 2022',
+        title: 'Matalec',
+        location: 'Madrid, Spain',
+        cta: 'meet Interflux at boot B302'
+      },
+      {
+        date: '6 Dec 2022',
+        title: 'Electronex',
+        location: 'Melbourne, Australia',
+        cta: 'meet Oritech at stand 24'
+      }
+    ];
+  }
+}
