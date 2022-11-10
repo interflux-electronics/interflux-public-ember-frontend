95b38601d5d08c9f4fdb110bfdf3fa8e2ec06cd4
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-11-10 23:26:20 +1100

    Implement event caching to prevent more than one load per session

--- app/pods/components/events/component.js
+++ app/pods/components/events/component.js
@@ -6,74 +6,20 @@ export default class EventsComponent extends Component {
   @service cache;
   @service store;
 
+  @tracked events;
+
   constructor() {
     super(...arguments);
-
     this.loadEvents();
   }
 
   async loadEvents() {
-    this.events = this.cache.events || (await this.store.findAll('event'));
-  }
-
-  @tracked events;
-
-  get eventssss() {
-    return [
-      {
-        date: '28 Oct 2022',
-        title: 'Ersa soldering technology seminar',
-        location: 'Munich, Germany',
-        cta: 'meet Steven Teliszewski'
-      },
-      {
-        date: '12 – 14 Nov 2022',
-        title: 'Matalec',
-        location: 'Madrid, Spain',
-        cta: 'meet Interflux at boot B302'
-      },
-      {
-        date: '6 Dec 2022',
-        title: 'Electronex',
-        location: 'Melbourne, Australia',
-        cta: 'meet Oritech at stand 24'
-      },
-      {
-        date: '28 Oct 2022',
-        title: 'Ersa soldering technology seminar',
-        location: 'Munich, Germany',
-        cta: 'meet Steven Teliszewski'
-      },
-      {
-        date: '12 – 14 Nov 2022',
-        title: 'Matalec',
-        location: 'Madrid, Spain',
-        cta: 'meet Interflux at boot B302'
-      },
-      {
-        date: '6 Dec 2022',
-        title: 'Electronex',
-        location: 'Melbourne, Australia',
-        cta: 'meet Oritech at stand 24'
-      },
-      {
-        date: '28 Oct 2022',
-        title: 'Ersa soldering technology seminar',
-        location: 'Munich, Germany',
-        cta: 'meet Steven Teliszewski'
-      },
-      {
-        date: '12 – 14 Nov 2022',
-        title: 'Matalec',
-        location: 'Madrid, Spain',
-        cta: 'meet Interflux at boot B302'
-      },
-      {
-        date: '6 Dec 2022',
-        title: 'Electronex',
-        location: 'Melbourne, Australia',
-        cta: 'meet Oritech at stand 24'
-      }
-    ];
+    if (this.cache.events) {
+      this.events = this.cache.events;
+      return;
+    }
+    const events = await this.store.findAll('event');
+    this.cache.events = events;
+    this.events = events;
   }
 }
