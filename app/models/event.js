db18627984389766809a3b069a290d8cd7629926
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-11-13 09:48:32 +1000

    Swap events timeline for vertical list in <Section::Events> component

--- app/models/event.js
+++ app/models/event.js
@@ -1,21 +1,10 @@
-import Model, { attr } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 export default class EventModel extends Model {
   @attr('string') name;
-  @attr('string') startDate;
-  @attr('string') endDate;
-  @attr('string') location;
-  @attr('string') callToAction;
+  @attr('string') dates;
+  @attr('string') city;
+  @attr('string') description;
 
-  get date() {
-    // is one day event
-    if (this.startDate === this.endDate) {
-      return this.startDate;
-    }
-    // end date is in same month as start date
-    if (this.startDate.split('/')[1] === this.endDate.split('/')[1]) {
-      return `${this.startDate.split('/')[1]} - ${this.endDate}`;
-    }
-    return `${this.startDate} - ${this.endDate}`;
-  }
+  @belongsTo('country') country;
 }
