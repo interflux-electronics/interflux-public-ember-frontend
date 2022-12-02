437d67b868974efe1ac31549d459e95b32fc0023
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-12-02 14:42:58 +1000

    Add guards to <Tag> component

--- app/helpers/lowercase.js
+++ app/helpers/lowercase.js
@@ -1,6 +1,13 @@
 import { helper } from '@ember/component/helper';
 
 export function lowercase(params) {
+  const phrase = params[0];
+
+  if (!phrase) {
+    console.warn('no phrase passed into {{lowercase ...}} helper');
+    return '';
+  }
+
   return params[0].toLowerCase();
 }
 
--- app/pods/components/tag/component.js
+++ app/pods/components/tag/component.js
@@ -4,6 +4,18 @@ import { htmlSafe } from '@ember/template';
 export default class TagComponent extends Component {
   get textWithWordSpans() {
     const phrase = this.args.text;
+
+    if (!phrase) {
+      console.warn('no @text passed into <Tag>');
+      return null;
+    }
+
+    if (typeof phrase !== 'string') {
+      console.warn('no valid string passed into <Tag>');
+      console.warn(phrase);
+      return null;
+    }
+
     const words = phrase.split(' ');
 
     const html = `<nobr><span class="word">${words.join(
