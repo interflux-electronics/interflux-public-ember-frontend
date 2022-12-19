801234f56f99ed32408969572471cb2f5b9d7bf5
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-12-19 12:02:53 +1000

    Replace hard coded English with dynamic translations in the <Header> component

--- app/pods/components/header/desktop/menu/processes/component.js
+++ app/pods/components/header/desktop/menu/processes/component.js
@@ -1,103 +1,111 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class HeaderDesktopMenuProcessesComponent extends Component {
-  @tracked processes = [
-    {
-      label: 'Low melting point soldering – LMPA™',
-      icon: 'LMPA-icon',
-      routeModel: 'for-low-melting-point-soldering'
-    },
-    {
-      label: 'OSP soldering',
-      icon: 'OSP-soldering-green',
-      routeModel: 'for-OSP-soldering'
-    },
-    {
-      label: 'Selective soldering',
-      icon: 'selective-soldering',
-      routeModel: 'for-selective-soldering'
-    },
-    {
-      label: 'Jet fluxing',
-      icon: 'jet-fluxing',
-      routeModel: 'for-jet-fluxing'
-    },
-    {
-      label: 'Hand soldering',
-      icon: 'rework-and-repair',
-      routeModel: 'for-hand-soldering'
-    },
-    {
-      label: 'Wave soldering',
-      icon: 'wave-soldering',
-      routeModel: 'for-wave-soldering'
-    },
-    {
-      label: 'Solder paste jetting',
-      icon: 'solder-paste-jetting',
-      routeModel: 'for-solder-paste-jetting'
-    },
-    {
-      label: 'Robot soldering',
-      icon: 'robot-soldering',
-      routeModel: 'for-robot-soldering'
-    },
-    {
-      label: 'Reflow soldering',
-      icon: 'reflow-soldering',
-      routeModel: 'for-reflow-soldering'
-    },
-    {
-      label: 'Spray fluxing',
-      icon: 'spray-fluxing',
-      routeModel: 'for-spray-fluxing'
-    },
-    {
-      label: 'Laser soldering',
-      icon: 'laser-soldering',
-      routeModel: 'for-laser-soldering'
-    },
-    {
-      label: 'Stencil printing',
-      icon: 'stencil-printing',
-      routeModel: 'for-stencil-printing'
-    },
-    {
-      label: 'Foam fluxing',
-      icon: 'foam-fluxing',
-      routeModel: 'for-foam-fluxing'
-    },
-    {
-      label: 'Dispensing',
-      icon: 'dispensing',
-      routeModel: 'for-dispensing'
-    },
-    {
-      label: 'Rework & repair',
-      icon: 'rework-and-repair',
-      routeModel: 'for-rework-and-repair'
-    },
-    {
-      label: 'Pre-tinning',
-      icon: 'pre-tinning',
-      routeModel: 'for-pre-tinning'
-    },
-    {
-      label: 'Solder bath conditioning',
-      icon: 'dip-soldering',
-      routeModel: 'for-solder-bath-conditioning'
-    },
-    {
-      label: 'Vapor phase soldering',
-      icon: 'vapour-phase-soldering',
-      routeModel: 'for-vapor-phase-soldering'
-    },
-    {
-      label: 'Dip soldering',
-      icon: 'dip-soldering',
-      routeModel: 'for-dip-soldering'
-    },
-    { label: 'Cleaning', icon: 'drop-3', routeModel: 'for-general-cleaning' }
-  ];
+  @service i18n;
+
+  get processes() {
+    return [
+      {
+        label: this.i18n.t('ui.header.processes.lmpa'),
+        icon: 'LMPA-icon',
+        routeModel: 'for-low-melting-point-soldering'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.osp'),
+        icon: 'OSP-soldering-green',
+        routeModel: 'for-OSP-soldering'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.selective'),
+        icon: 'selective-soldering',
+        routeModel: 'for-selective-soldering'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.jet-fluxing'),
+        icon: 'jet-fluxing',
+        routeModel: 'for-jet-fluxing'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.hand'),
+        icon: 'rework-and-repair',
+        routeModel: 'for-hand-soldering'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.wave'),
+        icon: 'wave-soldering',
+        routeModel: 'for-wave-soldering'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.jet-pasting'),
+        icon: 'solder-paste-jetting',
+        routeModel: 'for-solder-paste-jetting'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.robot'),
+        icon: 'robot-soldering',
+        routeModel: 'for-robot-soldering'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.reflow'),
+        icon: 'reflow-soldering',
+        routeModel: 'for-reflow-soldering'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.spray'),
+        icon: 'spray-fluxing',
+        routeModel: 'for-spray-fluxing'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.laser'),
+        icon: 'laser-soldering',
+        routeModel: 'for-laser-soldering'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.stencil'),
+        icon: 'stencil-printing',
+        routeModel: 'for-stencil-printing'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.foam'),
+        icon: 'foam-fluxing',
+        routeModel: 'for-foam-fluxing'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.dispensing'),
+        icon: 'dispensing',
+        routeModel: 'for-dispensing'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.rework'),
+        icon: 'rework-and-repair',
+        routeModel: 'for-rework-and-repair'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.pre-tinning'),
+        icon: 'pre-tinning',
+        routeModel: 'for-pre-tinning'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.solder-bath'),
+        icon: 'dip-soldering',
+        routeModel: 'for-solder-bath-conditioning'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.vapor'),
+        icon: 'vapour-phase-soldering',
+        routeModel: 'for-vapor-phase-soldering'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.dip'),
+        icon: 'dip-soldering',
+        routeModel: 'for-dip-soldering'
+      },
+      {
+        label: this.i18n.t('ui.header.processes.cleaning'),
+        icon: 'drop-3',
+        routeModel: 'for-general-cleaning'
+      }
+    ];
+  }
 }
