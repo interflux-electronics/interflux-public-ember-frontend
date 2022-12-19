5f7479877b63b8fb65119eb5ca8a9d9b2b5debf8
Jan Werkhoven <jan.werkhoven@gmail.com>
2022-12-19 12:04:15 +1000

    Make the application route first fetch translations

--- app/pods/application/route.js
+++ app/pods/application/route.js
@@ -1,18 +1,25 @@
+import ENV from 'interflux/config/environment';
 import BaseRoute from 'interflux/pods/base/route';
 import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default class ApplicationRoute extends BaseRoute {
   @service store;
   @service session;
-  @service translation;
 
   beforeModel() {
     this.session.create();
   }
 
   model() {
-    this.translation.languages.forEach((language) => {
-      this.store.createRecord('language', language);
+    const locale = ENV.locale || 'en';
+
+    console.log({ locale });
+
+    return hash({
+      translations: this.store.query('translation', {
+        filter: { locale, key: '~^ui.' }
+      })
     });
   }
 }
