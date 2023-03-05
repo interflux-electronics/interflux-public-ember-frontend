b183a6ef87f607b827bc76299c733a4a6810cb1f
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-03-05 22:16:51 +1100

    Add missing translation locations on product subset routes

--- app/pods/product/controller.js
+++ app/pods/product/controller.js
@@ -47,8 +47,8 @@ export default class ProductController extends Controller {
     return this.model.product.uses.map((use) => {
       return {
         iconURL: use.get('iconURL'),
-        label: this.translation.t(use.get('label'), `use.1.${use.id}`),
-        content: this.translation.t(use.get('gist'), `use.2.${use.id}`)
+        label: this.translation.t(use.get('label'), `use.1.${use.get('id')}`),
+        content: this.translation.t(use.get('gist'), `use.2.${use.get('id')}`)
         // links: [
         //   {
         //     label: `Learn more about ${use.get('text')}`,
@@ -71,11 +71,11 @@ export default class ProductController extends Controller {
         iconURL: quality.get('iconURL'),
         label: this.translation.t(
           quality.get('label'),
-          `quality.1.${quality.id}`
+          `quality.1.${quality.get('id')}`
         ),
         content: this.translation.t(
           quality.get('gist'),
-          `quality.2.${quality.id}`
+          `quality.2.${quality.get('id')}`
         )
         // links: [
         //   {
