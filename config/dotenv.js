c83ac912ed435411d1815c25c7faa8c4e45a18f2
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-04-08 14:20:03 +1000

    Require .env LANGUAGE instead of DOMAIN

--- config/dotenv.js
+++ config/dotenv.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 module.exports = function (/* env */) {
   return {
-    clientAllowedKeys: ['DOMAIN'],
+    clientAllowedKeys: ['LANGUAGE'],
     fastbootAllowedKeys: [],
     failOnMissingKey: true,
     path: path.join(path.dirname(__dirname), '.env')
