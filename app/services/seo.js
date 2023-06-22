82dc1e8dc7026d1fd92974a7fdcbe55f1c1f6327
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-06-22 21:36:49 +1000

    Make the <head> metatags translatable

--- app/services/seo.js
+++ app/services/seo.js
@@ -8,8 +8,10 @@ export default class SeoService extends Service {
     return {
       canonicalPath: '/',
       title: 'Interflux Electronics',
-      description:
+      description: this.translation.t(
         'Interflux Electronics specialises in researching, developing and producing soldering materials for electronics manufacturers. Best known for soldering fluxes, solder pastes, solder wires and soldering alloys.',
+        'seo.1'
+      ),
       ogType: 'website',
       ogImagePath: '/images/public/og/og-homepage.jpg',
       ogImageAlt: 'Interflux Electronics',
@@ -21,13 +23,17 @@ export default class SeoService extends Service {
   get products() {
     return {
       canonicalPath: '/products',
-      title: 'Products',
-      description:
+      title: this.translation.t('Products', 'seo.2'),
+      description: this.translation.t(
         'See all the products we produce: soldering fluxes, solder pastes, solder wires, solder alloys, ... All the chemistry needed for soldering electronics.',
+        'seo.3'
+      ),
       ogType: 'website',
       ogImagePath: '/images/public/og/og-products.jpg',
-      ogImageAlt:
+      ogImageAlt: this.translation.t(
         'Interflux Electronics soldering fluxes, solder pastes, solder wires and solder alloys',
+        'seo.4'
+      ),
       ogImageWidth: '1200',
       ogImageHeight: '630'
     };
@@ -36,13 +42,17 @@ export default class SeoService extends Service {
   get product() {
     return {
       canonicalPath: '/products',
-      title: 'Products',
-      description:
+      title: this.translation.t('Product', 'seo.5'),
+      description: this.translation.t(
         'See all the products we produce: soldering fluxes, solder pastes, solder wires, solder alloys, ... All the chemistry needed for soldering electronics.',
+        'seo.6'
+      ),
       ogType: 'website',
       ogImagePath: '/images/public/og/og-products.jpg',
-      ogImageAlt:
+      ogImageAlt: this.translation.t(
         'Interflux Electronics soldering fluxes, solder pastes, solder wires and solder alloys',
+        'seo.7'
+      ),
       ogImageWidth: '1200',
       ogImageHeight: '630'
     };
@@ -51,12 +61,17 @@ export default class SeoService extends Service {
   get webinars() {
     return {
       canonicalPath: '/webinars',
-      title: 'Webinars',
-      description:
+      title: this.translation.t('Webinars', 'seo.8'),
+      description: this.translation.t(
         "See videos in which Steven Teliszewski's walks you through research and findings relevant for electronics manufacturers such as: chemical reliability, low melting point alloys, ...",
+        'seo.9'
+      ),
       ogType: 'website',
       ogImagePath: '/images/public/og/og-webinars.jpg',
-      ogImageAlt: 'Interflux Electronics video webinars',
+      ogImageAlt: this.translation.t(
+        'Interflux Electronics video webinars',
+        'seo.10'
+      ),
       ogImageWidth: '1200',
       ogImageHeight: '630'
     };
@@ -65,12 +80,17 @@ export default class SeoService extends Service {
   get company() {
     return {
       canonicalPath: '/company',
-      title: 'Company',
-      description:
+      title: this.translation.t('Company', 'seo.11'),
+      description: this.translation.t(
         'Learn more about the values, history and people behind Interflux Electronics.',
+        'seo.12'
+      ),
       ogType: 'website',
       ogImagePath: '/images/public/og/og-company.jpg',
-      ogImageAlt: 'Interflux Electronics headquarters in Belgium',
+      ogImageAlt: this.translation.t(
+        'Interflux Electronics headquarters in Belgium',
+        'seo.13'
+      ),
       ogImageWidth: '1200',
       ogImageHeight: '630'
     };
@@ -79,12 +99,17 @@ export default class SeoService extends Service {
   get documents() {
     return {
       canonicalPath: '/documents',
-      title: 'Documents',
-      description:
+      title: this.translation.t('Documents', 'seo.14'),
+      description: this.translation.t(
         'Request technical datasheets, MSDS, RoHS, REACH and other helpful documents.',
+        'seo.15'
+      ),
       ogType: 'website',
       ogImagePath: '/images/public/og/og-documents.jpg',
-      ogImageAlt: 'Interflux Electronics documents',
+      ogImageAlt: this.translation.t(
+        'Interflux Electronics documents',
+        'seo.16'
+      ),
       ogImageWidth: '1200',
       ogImageHeight: '630'
     };
@@ -93,12 +118,17 @@ export default class SeoService extends Service {
   get contact() {
     return {
       canonicalPath: '/contact',
-      title: 'Contact',
-      description:
+      title: this.translation.t('Contact', 'seo.17'),
+      description: this.translation.t(
         'Find the right person to talk to. The Interflux Network consists of 40 partners worldwide and is located in key regions for the electronics manufacturing industry.',
+        'seo.18'
+      ),
       ogType: 'website',
       ogImagePath: '/images/public/og/og-contact.jpg',
-      ogImageAlt: 'Contact Interflux Electronics world wide',
+      ogImageAlt: this.translation.t(
+        'Contact Interflux Electronics world wide',
+        'seo.19'
+      ),
       ogImageWidth: '1200',
       ogImageHeight: '630'
     };
@@ -107,13 +137,18 @@ export default class SeoService extends Service {
   // TODO: make this dynamic for every contact
   get map() {
     return {
-      canonicalPath: '/contact',
-      title: 'Contact',
-      description:
+      canonicalPath: '/partners/map',
+      title: this.translation.t('Partners', 'seo.20'),
+      description: this.translation.t(
         'Find the right person to talk to. The Interflux Network consists of 40 partners worldwide and is located in key regions for the electronics manufacturing industry.',
+        'seo.21'
+      ),
       ogType: 'website',
       ogImagePath: '/images/public/og/og-contact.jpg',
-      ogImageAlt: 'Contact Interflux Electronics world wide',
+      ogImageAlt: this.translation.t(
+        'Contact Interflux Electronics world wide',
+        'seo.22'
+      ),
       ogImageWidth: '1200',
       ogImageHeight: '630',
       loadMapBox: true // IMPORTANT
@@ -123,8 +158,8 @@ export default class SeoService extends Service {
   get error() {
     return {
       canonicalPath: '/',
-      title: '404 Not Found',
-      description: 'There is no page here.'
+      title: this.translation.t('404 Not Found', 'seo.23'),
+      description: this.translation.t('There is no page here.', 'seo.24')
     };
   }
 }
