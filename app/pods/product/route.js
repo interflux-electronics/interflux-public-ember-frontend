87747f0a6db7f0b93c7f954897e9988253b87c86
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-07-09 18:46:36 +1000

    Fix backbutton translation on product page

--- app/pods/product/route.js
+++ app/pods/product/route.js
@@ -12,6 +12,7 @@ export default class ProductRoute extends BaseRoute {
           'product_qualities',
           'product_uses',
           'product_family',
+          'product_family.product_family',
           'product_images',
           'product_images.image',
           'product_documents'
