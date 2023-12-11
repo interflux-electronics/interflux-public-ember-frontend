79363428de2a0c6015bbf6719b42f0128e966ed3
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-12-11 23:14:16 +1100

    Remove space from variations path

--- app/pods/company/route.js
+++ app/pods/company/route.js
@@ -17,7 +17,7 @@ export default class CompanyRoute extends BaseRoute {
         title: "Interflux Electronics's journey towards sustainability",
         path: 'videos/our-journey-towards-sustanability/our-journey-towards-sustanability',
         variations:
-          '@1920x1080.mp4,@1920x1080.webm,@1920x1080.ogg,@800x449.jpg, @800x449.jpg'
+          '@1920x1080.mp4,@1920x1080.webm,@1920x1080.ogg,@800x449.jpg,@800x449.jpg'
       })
     };
   }
