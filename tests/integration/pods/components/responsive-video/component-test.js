a0807b87d1a5113626f90fb49ed705a57fa4b0d5
Jan Werkhoven <jan.werkhoven@gmail.com>
2023-12-11 18:00:08 +1100

    Update video component test

--- tests/integration/pods/components/responsive-video/component-test.js
+++ tests/integration/pods/components/responsive-video/component-test.js
@@ -8,18 +8,26 @@ module('Integration | Component | responsive-video', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
-    assert.expect(3);
+    assert.expect(4);
 
     this.video = EmberObject.create({
       path: 'videos/test/foo',
-      variations: '@1920x1080.mp4,@1920x1080.webm'
+      variations: '@1920x1080.mp4,@1920x1080.webm',
+      posters: '@1920x1080.png,@1920x1080.webp'
     });
 
-    await render(hbs`<ResponsiveVideo @video={{this.video}} />`);
+    await render(
+      hbs`<ResponsiveVideo @path={{this.video.path}} @variations={{this.video.variations}} @posters={{this.video.posters}} />`
+    );
 
     const svg = this.element.querySelector('svg');
+    const video = this.element.querySelector('video');
 
-    assert.equal(svg.getAttribute('viewBox'), '0 0 16 9');
+    assert.strictEqual(svg.getAttribute('viewBox'), '0 0 16 9');
+    assert.strictEqual(
+      video.getAttribute('poster'),
+      'http://localhost:9000/videos/test/foo@1920x1080.webp'
+    );
     assert.ok(this.element.querySelector('source[type="video/webm"]'));
     assert.ok(this.element.querySelector('source[type="video/mp4"]'));
   });
