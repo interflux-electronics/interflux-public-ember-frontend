666c6dfd17220e0a20682d83ed22bcab332045a7
Jan Werkhoven <jan.werkhoven@gmail.com>
2024-04-07 22:52:21 +1000

    Add 2 routes for event registration modals

---
+++ app/pods/contact/event/route.js
@@ -0,0 +1,26 @@
+import ModalRoute from 'interflux/pods/components/modal/route';
+
+export default class ContactEventRoute extends ModalRoute {
+  model(params) {
+    return {
+      event: this.store.peekRecord('event', params.event_id)
+    };
+  }
+
+  afterModel(model) {
+    if (!model.event) {
+      console.warn('redirecting back to contact page');
+      this.router.transitionTo('contact');
+    }
+  }
+
+  // Properties on controllers will linger when switching between models.
+  // To avoid, we reset them each time the route is exited.
+  // https://api.emberjs.com/ember/3.24/classes/Route/methods?anchor=resetController
+  resetController(controller, isExiting, transition) {
+    if (isExiting && transition.targetName !== 'error') {
+      // TBC
+      // controller.set('query', null);
+    }
+  }
+}
--- app/router.js
+++ app/router.js
@@ -12,6 +12,7 @@ Router.map(function () {
   // /products/family/solder-pastes                        "Solder pastes"
   // /products/family/solder-pastes/for/stencil-printing   "Solder pastes for stencil printing"
   // /products/for/stencil-printing                        "For stencil printing"
+
   this.route('products', function () {
     this.route('family', { path: '/family/:main_family_id' });
     this.route('use', { path: '/for/:use_id' });
@@ -25,10 +26,13 @@ Router.map(function () {
   this.route('documents', function () {
     this.route('category', { path: '/:category' });
   });
-  this.route('contact');
+  this.route('contact', function () {
+    this.route('event', { path: '/event/:event_id' });
+  });
   this.route('partners', function () {
     this.route('map');
   });
+  this.route('event', { path: '/event/:event_id' });
   this.route('permalink', { path: '/QR/:code' });
   this.route('catchall', { path: '*:' });
 });
