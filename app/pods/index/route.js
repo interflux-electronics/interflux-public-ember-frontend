1dfe32323fc6116be692c000d37f71ee5e330641
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2025-05-19 19:19:04 +1000

    Make the index route resiliant for 500 errors

--- app/pods/index/route.js
+++ app/pods/index/route.js
@@ -1,5 +1,5 @@
 import BaseRoute from 'interflux/pods/base/route';
-import { hash } from 'rsvp';
+import { hash, hashSettled } from 'rsvp';
 
 export default class HomepageRoute extends BaseRoute {
   activate() {
@@ -10,12 +10,12 @@ export default class HomepageRoute extends BaseRoute {
     });
   }
 
-  model() {
+  async model() {
     if (this.cachedPayload) {
       return this.cachedPayload;
     }
 
-    const payload = {
+    const results = await hashSettled({
       products: this.store.query('product', {
         filter: { onFrontPage: true },
         include: 'main_family,sub_family'
@@ -23,10 +23,32 @@ export default class HomepageRoute extends BaseRoute {
       events: this.store.query('event', {
         include: 'country'
       })
-    };
+    });
 
-    return hash(payload);
+    return {
+      products:
+        results.products.state === 'fulfilled' ? results.products.value : null,
+      events: results.events.state === 'fulfilled' ? results.events.value : null
+    };
   }
+
+  // model() {
+  //   if (this.cachedPayload) {
+  //     return this.cachedPayload;
+  //   }
+
+  //   const payload = {
+  //     products: this.store.query('product', {
+  //       filter: { onFrontPage: true },
+  //       include: 'main_family,sub_family'
+  //     }),
+  //     events: this.store.query('event', {
+  //       include: 'country'
+  //     })
+  //   };
+
+  //   return hash(payload);
+  // }
 
   afterModel(model) {
     this.headData.update(this.seo.homepage(model.events));
